9e405af99901a43f54be4dbcb4dbcd3e2c80e074
mengyahuUSTC-PU <humengyahumy@gmail.com>
2024-08-08 15:24:23 -0700

    add the simple prompt running cmd in bash file

--- reason_analysis.sh
+++ reason_analysis.sh
@@ -6,4 +6,5 @@ dataname=$3
 testmode=$4
 python constrained_reasoner.py --groundingsource "$gs" --hyp "$hyp" --dataname "$dataname" --category true --testmode "$testmode"
 python constrained_reasoner.py --groundingsource "$gs" --hyp "$hyp" --dataname "$dataname" --category false --testmode "$testmode"
+python constrained_reasoner.py --groundingsource "$gs" --hyp "$hyp" --dataname "$dataname" --category false --simple true --testmode "$testmode"
 python analysis.py --dataname "$dataname" 
