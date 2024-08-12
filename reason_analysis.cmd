44eb90008433731ccec10a8be4a41f39f2d3297e
Yaxi Li <yaxli@microsoft.com>
2024-08-11 22:55:07 -0700

    Fix reason_analysis.cmd

--- reason_analysis.cmd
+++ reason_analysis.cmd
@@ -2,6 +2,6 @@ set gs=%1
 set hyp=%2
 set dataname=%3
 set testmode=%4
-python constrained_reasoner.py --groundingsource "%gs%" --hyp "%hyp%" --dataname "%dataname%" --category true --testmode "%testmode%"  
-python constrained_reasoner.py --groundingsource "%gs%" --hyp "%hyp%" --dataname "%dataname%" --category false --testmode "%testmode%"  
-python analysis.py --dataname "%dataname%"
+python constrained_reasoner.py --groundingsource %gs% --hyp %hyp% --dataname %dataname% --category true --testmode %testmode%  
+python constrained_reasoner.py --groundingsource %gs% --hyp %hyp% --dataname %dataname% --category false --testmode %testmode%  
+python analysis.py --dataname %dataname%
