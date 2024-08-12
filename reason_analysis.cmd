72816f08565b685475b690b650771b54654f4ea4
Yaxi Li <yaxli@microsoft.com>
2024-08-12 11:24:45 -0700

    Add RAGTruth results

--- reason_analysis.cmd
+++ reason_analysis.cmd
@@ -2,6 +2,7 @@ set gs=%1
 set hyp=%2
 set dataname=%3
 set testmode=%4
-python constrained_reasoner.py --groundingsource %gs% --hyp %hyp% --dataname %dataname% --category true --testmode %testmode%  
-python constrained_reasoner.py --groundingsource %gs% --hyp %hyp% --dataname %dataname% --category false --testmode %testmode%  
+python constrained_reasoner.py --groundingsource %gs% --hyp %hyp% --dataname %dataname% --category true --testmode %testmode% 
+python constrained_reasoner.py --groundingsource %gs% --hyp %hyp% --dataname %dataname% --category false --testmode %testmode%
+python constrained_reasoner.py --groundingsource %gs% --hyp %hyp% --dataname %dataname% --category false --simple true --testmode %testmode%
 python analysis.py --dataname %dataname%
